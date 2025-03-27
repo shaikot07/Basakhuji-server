14f575f8bd4fbcb507eb2adc81b7b63d69f05d99
shaikot07 <saifulislamshaikat007@gmail.com>
2025-03-27 22:03:43 +0600

    updated: cleanup user route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -5,9 +5,7 @@ import { USER_ROLE } from './user.constant';
 
 const router = express.Router();
 
-// router.post('/register',validateRequest(UserValidation.userValidationSchema), userController.newUserRegistration)
 
-// router.post('/login',validateRequest(AuthValidation.loginValidationSchema), AuthControllers.loginUser)
 // router.post(
 //   '/create-student',
 //   auth(USER_ROLE.admin),
